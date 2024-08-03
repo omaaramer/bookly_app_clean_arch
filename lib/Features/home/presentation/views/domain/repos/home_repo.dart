119729bc74d8c71c2632a7baef0205ee8fa05846
omaaramer <omahar1907@gmail.com>
2024-08-03 06:39:59 +0300

    add Either class to return either faluire or data

--- lib/Features/home/presentation/views/domain/repos/home_repo.dart
+++ lib/Features/home/presentation/views/domain/repos/home_repo.dart
@@ -1,6 +1,9 @@
+import 'package:bookly/core/errors/failure.dart';
+
 import '../entites/book_entity.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<List<BookEntity>> fetchFeaturedBooks();
-  Future<List<BookEntity>> fetchNewestBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks();
 }
